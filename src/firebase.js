e29476d480e5994ed971a0d27e1ab07d307b13b2
suryansh <suryanshsingh210@gmail.com>
2024-02-02 02:25:01 +0530

    login and signup page

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBYobTtU4TF1o5Mw9OCHFy5zakmL2AyIlg",
+    authDomain: "user-management-fafef.firebaseapp.com",
+    projectId: "user-management-fafef",
+    storageBucket: "user-management-fafef.appspot.com",
+    messagingSenderId: "909953360897",
+    appId: "1:909953360897:web:ccdc6d0061e744184c8c00",
+    measurementId: "G-8LX4T0KKYW"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
